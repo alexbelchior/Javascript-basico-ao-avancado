22a4be148d89d9693ee09cb3e4034f4ff0f69905
alexbelchior <alexbelchior@gmail.com>
2023-08-07 23:02:24 -0300

    Operacoes aritmeticas

---
+++ 2 Operacoes Aritmeticas/script.js
@@ -0,0 +1,6 @@
+console.log("Soma de 2 + 2 = ",  2 + 2);
+console.log("Subtração de 5 - 3 = ", 5 - 3);
+console.log("Multiplicação de 3 x 3 = ", 3 * 3);
+console.log("Divisão de 6 / 2 = ", 6 / 2);
+console.log("Resto da divisão de 6 / 2 = ", 6 % 2);
+console.log("Resto da divisão de 7 / 2 = ", 7 % 2);
